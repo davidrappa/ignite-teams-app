6768c26e2f13e52d3b5c3e2f1f8093eff503737a
David Rappa <davidrappa@outlook.com>
2023-02-04 08:51:32 -0300

    feat(Components): added index.ts to export default component

---
+++ src/components/index.ts
@@ -0,0 +1,10 @@
+export { default as Button } from "./Button";
+export { default as ButtonIcon } from "./ButtonIcon";
+export { default as Filter } from "./Filter";
+export { default as GroupCard } from "./GroupCard";
+export { default as Header } from "./Header";
+export { default as Highlight } from "./Highlight";
+export { default as Input } from "./Input";
+export { default as ListEmpty } from "./ListEmpty";
+export { default as Loading } from "./Loading";
+export { default as PlayerCard } from "./PlayerCard";

5e1205c99b773f54f5c21fad40b93b1c18dc9687
David Rappa <davidrappa@outlook.com>
2023-02-04 08:56:09 -0300

    feat(Screens): created Players screen

---
+++ src/screens/Players/index.tsx
@@ -0,0 +1,64 @@
+import React, { useState } from "react";
+import { FlatList } from "react-native";
+
+import {
+  Header,
+  Highlight,
+  ButtonIcon,
+  Input,
+  Filter,
+  PlayerCard,
+  ListEmpty,
+  Button,
+} from "@components/index";
+
+import * as S from "./styles";
+
+const Players = (): JSX.Element => {
+  const [team, setTeam] = useState("Time A");
+  const [players, setPlayers] = useState([]);
+
+  return (
+    <S.Container>
+      <Header showBackButton />
+      <Highlight title="Nome da turma" subtitle="adicione a galera" />
+      <S.Form>
+        <Input placeholder="Nome da pessoa" autoCorrect={false} />
+        <ButtonIcon icon="add" />
+      </S.Form>
+      <S.HeaderList>
+        <FlatList
+          data={["Time A", "Time B"]}
+          keyExtractor={(item) => item}
+          renderItem={({ item }) => (
+            <Filter
+              title={item}
+              isActive={item === team}
+              onPress={() => setTeam(item)}
+            />
+          )}
+          horizontal
+        />
+        <S.NumberOfPlayers>{players.length}</S.NumberOfPlayers>
+      </S.HeaderList>
+      <FlatList
+        data={players}
+        showsVerticalScrollIndicator={false}
+        keyExtractor={(item) => item}
+        renderItem={({ item }) => (
+          <PlayerCard name={item} onRemove={() => {}} />
+        )}
+        ListEmptyComponent={() => (
+          <ListEmpty message="Não a pessoas neste time" />
+        )}
+        contentContainerStyle={[
+          { paddingBottom: 100 },
+          players.length === 0 && { flex: 1 },
+        ]}
+      />
+      <Button title="Remover Turma" type="secondary" />
+    </S.Container>
+  );
+};
+
+export default Players;

0e227c4f3f88f05b45b66b32d02d2e79f3f7cbb5
David Rappa <davidrappa@outlook.com>
2023-02-08 07:59:28 -0300

    feat(NewGroup): added function to pass group name in routes to next screen

--- src/screens/NewGroup/index.tsx
+++ src/screens/NewGroup/index.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback } from "react";
+import React, { useCallback, useState } from "react";
 import { useNavigation } from "@react-navigation/native";
 
 import { Header, Highlight, Button, Input } from "@components/index";
@@ -6,11 +6,13 @@ import { Header, Highlight, Button, Input } from "@components/index";
 import * as S from "./styles";
 
 const NewGroup = (): JSX.Element => {
+  const [group, setGroup] = useState("");
+
   const navigation = useNavigation();
 
   const handleNew = useCallback(() => {
-    navigation.navigate("Players", { group: "" });
-  }, []);
+    navigation.navigate("Players", { group });
+  }, [group]);
 
   return (
     <S.Container>
@@ -21,7 +23,11 @@ const NewGroup = (): JSX.Element => {
           title="Nova turma"
           subtitle="crie a turma para adicionar as pessoas"
         />
-        <Input placeholder="Nome da turma" />
+        <Input
+          placeholder="Nome da turma"
+          value={group}
+          onChangeText={setGroup}
+        />
         <Button title="Criar" style={{ marginTop: 20 }} onPress={handleNew} />
       </S.Content>
     </S.Container>

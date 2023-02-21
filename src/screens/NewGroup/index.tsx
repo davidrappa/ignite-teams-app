854dc7c88970519d136bbc72c4098b0c60cecb0c
David Rappa <davidrappa@outlook.com>
2023-02-21 10:21:05 -0300

    feat(NewGroup): added function handleNew to add new group inside screen

--- src/screens/NewGroup/index.tsx
+++ src/screens/NewGroup/index.tsx
@@ -1,7 +1,12 @@
 import React, { useCallback, useState } from "react";
 import { useNavigation } from "@react-navigation/native";
+import { Alert } from "react-native";
 
 import { Header, Highlight, Button, Input } from "@components/index";
+
+import { groupCreate } from "@storage/group/groupCreate";
+
+import { AppError } from "@utils/AppError";
 
 import * as S from "./styles";
 
@@ -10,9 +15,23 @@ const NewGroup = (): JSX.Element => {
 
   const navigation = useNavigation();
 
-  const handleNew = useCallback(() => {
-    navigation.navigate("Players", { group });
-  }, [group]);
+  const handleNew = useCallback(async () => {
+    try {
+      if (group.trim().length === 0) {
+        return Alert.alert("Novo Grupo", "Informe o nome da turma");
+      }
+
+      await groupCreate(group);
+
+      navigation.navigate("Players", { group });
+    } catch (error) {
+      if (error instanceof AppError) {
+        Alert.alert("Novo Grupo", error.message);
+      } else {
+        Alert.alert("Novo Grupo", "Não foi possivel criar um grupo");
+      }
+    }
+  }, [navigation, group]);
 
   return (
     <S.Container>

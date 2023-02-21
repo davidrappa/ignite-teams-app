ffd08484f68950cdb559044abc8138b55d663b4f
David Rappa <davidrappa@outlook.com>
2023-02-21 10:20:04 -0300

    feat(Input): added prop ref in input component

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,12 +1,22 @@
 import React, { memo } from "react";
-import { TextInputProps } from "react-native";
+import { TextInput, TextInputProps } from "react-native";
 import { useTheme } from "styled-components/native";
 
 import * as S from "./styles";
 
-const Input = ({ ...rest }: TextInputProps): JSX.Element => {
+type Props = TextInputProps & {
+  inputRef?: React.RefObject<TextInput>;
+};
+
+const Input = ({ inputRef, ...rest }: Props): JSX.Element => {
   const { COLORS } = useTheme();
-  return <S.Container {...rest} placeholderTextColor={COLORS.GRAY_300} />;
+  return (
+    <S.Container
+      ref={inputRef}
+      {...rest}
+      placeholderTextColor={COLORS.GRAY_300}
+    />
+  );
 };
 
 export default memo(Input);
